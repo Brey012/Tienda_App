17b63bddfbb88d69f48a937864f8eb35e7a9c7f5
Breyner012 <bhernandezos@cesde.net>
2025-03-25 11:13:38 -0500

    Creación de la tabla Pedido y su enum correspondiente

---
+++ src/main/java/TiendaApp/demo/modulos/Pedido.java
@@ -0,0 +1,69 @@
+package TiendaApp.demo.modulos;
+
+import TiendaApp.demo.Enums.EnumEstadoPedido;
+import jakarta.persistence.*;
+
+import java.util.Date;
+
+@Entity
+@Table(name = "Pedido")
+public class Pedido {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_pedido")
+    private int id_pedido;
+
+    // FK id_usuario
+
+    // FK id_tienda
+
+    @Column(name = "estado_pedido", length = 100, nullable = false)
+    private EnumEstadoPedido estado_pedido;
+    @Column(name = "fecha_pedido", length = 100, nullable = false)
+    private Date fecha_pedido;
+    @Column(name = "total_pedito", length = 100, nullable = false)
+    private double total_pedido;
+
+    public Pedido() {
+    }
+
+    public Pedido(int id_pedido, EnumEstadoPedido estado_pedido, Date fecha_pedido, double total_pedido) {
+        this.id_pedido = id_pedido;
+        this.estado_pedido = estado_pedido;
+        this.fecha_pedido = fecha_pedido;
+        this.total_pedido = total_pedido;
+    }
+
+    public int getId_pedido() {
+        return id_pedido;
+    }
+
+    public void setId_pedido(int id_pedido) {
+        this.id_pedido = id_pedido;
+    }
+
+    public EnumEstadoPedido getEstado_pedido() {
+        return estado_pedido;
+    }
+
+    public void setEstado_pedido(EnumEstadoPedido estado_pedido) {
+        this.estado_pedido = estado_pedido;
+    }
+
+    public Date getFecha_pedido() {
+        return fecha_pedido;
+    }
+
+    public void setFecha_pedido(Date fecha_pedido) {
+        this.fecha_pedido = fecha_pedido;
+    }
+
+    public double getTotal_pedido() {
+        return total_pedido;
+    }
+
+    public void setTotal_pedido(double total_pedido) {
+        this.total_pedido = total_pedido;
+    }
+}
