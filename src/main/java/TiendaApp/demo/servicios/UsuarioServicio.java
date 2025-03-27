f1a95a9399728068ee2d5b8de48dba16d04177e5
Breyner012 <bhernandezos@cesde.net>
2025-03-27 11:34:43 -0500

    Creación Del metodo Guardar

--- src/main/java/TiendaApp/demo/servicios/UsuarioServicio.java
+++ src/main/java/TiendaApp/demo/servicios/UsuarioServicio.java
@@ -1,6 +1,9 @@
 package TiendaApp.demo.servicios;
 
+import TiendaApp.demo.modulos.Usuario;
 import TiendaApp.demo.repositorios.IUsuario;
+import jakarta.persistence.Id;
+import org.apache.catalina.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -8,4 +11,23 @@ import org.springframework.stereotype.Service;
 public class UsuarioServicio {
     @Autowired
     private IUsuario repositorio;
+
+    //Listar los metodos que activaran las consultas en la BD
+
+    // Guardar
+    public Usuario guadarUsuario(Usuario datosUsuario) throws Exception {
+        try {
+            // Validar los datos de entrada
+            return this.repositorio.save(datosUsuario);
+        } catch(Exception error) {
+            throw new Exception(error.getMessage());
+        }
+    }
+    // Buscar Registros
+
+    // Buscar por ID
+
+    // Modificar por ID
+
+    // Eliminar por ID
 }
